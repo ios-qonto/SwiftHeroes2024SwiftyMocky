7eaf3c88af96e4b892944493478f9bbad61c6183
Davide Tamiazzo <davide.tamiazzo@qonto.com>
2024-01-16 14:57:18 +0100

    Initial Commit

---
+++ SwiftHeroes2024SwiftyMocky/SwiftHeroes2024SwiftyMocky.h
@@ -0,0 +1,18 @@
+//
+//  SwiftHeroes2024SwiftyMocky.h
+//  SwiftHeroes2024SwiftyMocky
+//
+//  Created by Davide Tamiazzo on 16/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftHeroes2024SwiftyMocky.
+FOUNDATION_EXPORT double SwiftHeroes2024SwiftyMockyVersionNumber;
+
+//! Project version string for SwiftHeroes2024SwiftyMocky.
+FOUNDATION_EXPORT const unsigned char SwiftHeroes2024SwiftyMockyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHeroes2024SwiftyMocky/PublicHeader.h>
+
+
