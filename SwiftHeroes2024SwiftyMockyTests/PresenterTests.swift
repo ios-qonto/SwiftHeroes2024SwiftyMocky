9193da74a1f83e105dd2d8b1d076755f13b34083
Davide Tamiazzo <davide.tamiazzo@qonto.com>
2024-01-18 17:05:32 +0100

    Updated PresenterTests to check displayer behavior as well

--- SwiftHeroes2024SwiftyMockyTests/PresenterTests.swift
+++ SwiftHeroes2024SwiftyMockyTests/PresenterTests.swift
@@ -12,7 +12,7 @@ final class PresenterTests: XCTestCase {
     private let useCase = UseCaseMock()
     private let displayer = DisplayerMock()
 
-    func test_onViewDidLoad() async throws {
+    func test_onViewDidLoad_givenId_thenCallsUseCase() async throws {
         // GIVEN
         let id = "id"
         let underTest = Presenter(id: id, useCase: useCase, displayer: displayer)
@@ -24,5 +24,22 @@ final class PresenterTests: XCTestCase {
         // THEN
         await task.value
         useCase.verify(.getContent(id: .value(id)))
+    }
+
+    func test_onViewDidLoad_givenOtherId_thenUpdatesDisplayerWithOtherContent() async throws {
+        // GIVEN
+        let id = "id"
+        let otherId = "otherId"
+        let otherContent = "OtherContent"
+        let underTest = Presenter(id: otherId, useCase: useCase, displayer: displayer)
+        useCase.given(.getContent(id: .value(id), willReturn: "Content"))
+        useCase.given(.getContent(id: .value(otherId), willReturn: otherContent))
+
+        // WHEN
+        let task = underTest.onViewDidLoad()
+
+        // THEN
+        await task.value
+        displayer.verify(.display(text: .value(otherContent)))
     }
 }
